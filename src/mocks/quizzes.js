4e6b710b4705378ce98da0f461d91a2e763b6105
nacles <juan.sobrino@oneclick.es>
2021-11-16 13:09:38 +0100

    Mock for quizzes

---
+++ src/mocks/quizzes.js
@@ -0,0 +1,17 @@
+export const quizzesMock = [
+  {
+    name: "Cute and lonely animals",
+    description: "This is the description for the Animal quiz",
+    exercises: [],
+  },
+  {
+    name: "Great cities of the world",
+    description: "This is the description for the Cities quiz",
+    exercises: [],
+  },
+  {
+    name: "A garden full of flowers",
+    description: "This is the description for the Flowers quiz",
+    exercises: [],
+  },
+];
