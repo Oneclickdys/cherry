3f1a788738aebba261be30e80015754598b7558b
nacles <juan.sobrino@oneclick.es>
2021-11-17 15:11:31 +0100

    import Code css in index

--- src/modules/views/Code/Code.js
+++ src/modules/views/Code/Code.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import { useAppContext } from '../../../context/AppContext';
 import BlockHeader from '../../atoms/BlockHeader/BlockHeader';
 import BoxCode from '../../atoms/BoxCode/BoxCode';
 import Button from '../../atoms/Button/Button';
 import VerticalCenterLayout from '../../atoms/VerticalCenterLayout/VerticalCenterLayout';
 import QuizTitle from '../../components/QuizTitle/QuizTitle';
 import useCode from './useCode';
-import './_code.scss';
-import { useAppContext } from '../../../context/AppContext';
 
 const data = {
   quizTitle: 'Título del quiz',
@@ -18,18 +17,18 @@ const data = {
 const Code = () => {
   const { code, users, onStartGame } = useCode();
   const { quizCode, setQuizCode } = useAppContext();
-  window.setQuizCode = (val) => setQuizCode(val)
+  window.setQuizCode = (val) => setQuizCode(val);
   return (
     <VerticalCenterLayout>
-      <div className='align-center'>
+      <div className="align-center">
         <div>context: {quizCode}</div>
         <QuizTitle title={data.quizTitle} subtitle={data.quizSubtitle} />
         <BoxCode>{code}</BoxCode>
         <BlockHeader>{data.listHeader}</BlockHeader>
-        <ul className='user-list'>
-          {users.map((user) =>
-            <li className='user-list__name'>{user.name}</li>,
-          )}
+        <ul className="user-list">
+          {users.map((user) => (
+            <li className="user-list__name">{user.name}</li>
+          ))}
         </ul>
         <Button onClick={onStartGame}>Comenzar</Button>
       </div>
