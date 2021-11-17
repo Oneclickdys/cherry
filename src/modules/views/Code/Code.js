5b07d3d89a300f1e48377b7573721cf4cd94409d
jany <janire.ng@gmail.com>
2021-11-17 14:04:40 +0100

    conectado botón de comenzar

--- src/modules/views/Code/Code.js
+++ src/modules/views/Code/Code.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import useCode from './useCode';
-import QuizTitle from '../../components/QuizTitle/QuizTitle';
-import BoxCode from '../../atoms/BoxCode/BoxCode';
 import BlockHeader from '../../atoms/BlockHeader/BlockHeader';
-import VerticalCenterLayout from '../../atoms/VerticalCenterLayout/VerticalCenterLayout';
+import BoxCode from '../../atoms/BoxCode/BoxCode';
 import Button from '../../atoms/Button/Button';
+import VerticalCenterLayout from '../../atoms/VerticalCenterLayout/VerticalCenterLayout';
+import QuizTitle from '../../components/QuizTitle/QuizTitle';
+import useCode from './useCode';
 import './_code.scss';
 
 const data = {
@@ -18,16 +18,16 @@ const Code = () => {
   const { code, users, onStartGame } = useCode();
   return (
     <VerticalCenterLayout>
-      <div className='align-center'>
+      <div className="align-center">
         <QuizTitle title={data.quizTitle} subtitle={data.quizSubtitle} />
         <BoxCode>{code}</BoxCode>
         <BlockHeader>{data.listHeader}</BlockHeader>
-        <ul className='user-list'>
-          {users.map((user) =>
-            <li className='user-list__name'>{user.name}</li>,
-          )}
+        <ul className="user-list">
+          {users.map((user) => (
+            <li className="user-list__name">{user.name}</li>
+          ))}
         </ul>
-        <Button>Comenzar</Button>
+        <Button onClick={onStartGame}>Comenzar</Button>
       </div>
     </VerticalCenterLayout>
   );
