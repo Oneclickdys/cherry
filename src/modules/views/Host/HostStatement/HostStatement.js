619f4d954587f60028a441a68a59668c043cb332
nacles <juan.sobrino@oneclick.es>
2021-12-01 13:52:11 +0100

    Host statement

--- src/modules/views/Host/HostStatement/HostStatement.js
+++ src/modules/views/Host/HostStatement/HostStatement.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ProgressBar from '../../../atoms/ProgressBar/ProgressBar';
 import useHostStatement from './useHostStatement';
 
 const HostStatement = () => {
-  const { currentPage } = useHostStatement();
+  const { statement, progress } = useHostStatement();
 
-  return <div>{currentPage?.currentQuestion?.statement}</div>;
+  return (
+    <div className="host-statement">
+      {statement && (
+        <div className="host-statement__wrapper">
+          <div className="host-statement__title">{statement}</div>
+          <div className="host-statement__progress">
+            <ProgressBar progress={progress} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HostStatement;
--- src/modules/views/Host/HostStatement/useHostStatement.js
+++ src/modules/views/Host/HostStatement/useHostStatement.js
@@ -3,28 +3,52 @@ import { useNavigate } from 'react-router';
 import { useAppContext } from '../../../../context/AppContext';
 import { getOnceCurrentPage, putCurrentPage } from '../../../../server/firebase';
 import { PAGES } from '../../../../utils/constants';
+import { calculateAverageTimeToReadStatement, percentage, stripHtml } from '../../../../utils/general';
 
 export default function useHostStatement() {
-  const [currentPage, setCurrentPage] = useState(null);
-  const { gameCode, currentQuiz } = useAppContext();
   let navigate = useNavigate();
+
+  const [currentPage, setCurrentPage] = useState(null);
+  const [statement, setStatement] = useState('');
+  const [timeToRead, setTimeToRead] = useState(null);
+  const [actualSecond, setActualSecond] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  const { gameCode, currentQuiz } = useAppContext();
 
   useEffect(() => {
     getCurrentPage();
   }, []);
 
-  const goQuestion = (page) => {
-    putCurrentPage(gameCode, PAGES.question, currentQuiz.questions[page.indexQuestion], page.indexQuestion);
-    navigate(`/question`);
-  };
+  useEffect(() => {
+    setTimeToRead(calculateAverageTimeToReadStatement(statement));
+  }, [statement]);
+
+  useEffect(() => {
+    const timer = actualSecond < timeToRead && setInterval(() => setActualSecond(actualSecond + 1), 1000);
+    return () => clearInterval(timer);
+  }, [timeToRead, actualSecond]);
+
+  useEffect(() => {
+    setProgress(percentage(actualSecond, timeToRead));
+
+    if (actualSecond === timeToRead) {
+      setTimeout(() => {
+        if (actualSecond === timeToRead) goQuestion(currentPage);
+      }, 1000);
+    }
+  }, [actualSecond]);
 
   async function getCurrentPage() {
     const page = await getOnceCurrentPage(gameCode);
     setCurrentPage(page);
-    setTimeout(() => {
-      goQuestion(page);
-    }, 4000);
+    setStatement(stripHtml(page?.currentQuestion?.data?.stimulus));
   }
 
-  return { currentPage };
+  function goQuestion(page) {
+    putCurrentPage(gameCode, PAGES.question, currentQuiz.questions[page.indexQuestion], page.indexQuestion);
+    navigate(`/question`);
+  }
+
+  return { statement, progress };
 }
